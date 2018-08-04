2c2df6b71722258df336373d9c30a6884980e391
XuRuHang <xu.ruhang@gmail.com>
2018-08-04 10:06:45 +0200

    Ajout gestion des actions pour l'utilisateur

--- js/contacts.js
+++ js/contacts.js
@@ -19,3 +19,34 @@ listeContacts.push(contact1);
 var contact2 = Object.create(Contact);
 contact2.init("Mélodie", "Nelsonne");
 listeContacts.push(contact2);
+
+var saisie = '';
+while (saisie !== "0") {
+    console.log("");
+    console.log("1 : Lister les contacts");
+    console.log("2 : Ajouter un contact");
+    console.log("0 : Quitter");
+    saisie = prompt("Votre choix:");
+
+    switch (saisie) {
+        case "1":
+            console.log("Voici la liste de tous vos contacts:");
+            listeContacts.forEach(function (contact) {
+                console.log(contact.decrire());
+            });
+            break;
+        case "2":
+            var saisieNom = prompt("Donner nom:");
+            var saisiePrenom = prompt("Donner prénom:");
+            var nouveauContact = Object.create(Contact);
+            nouveauContact.init(saisiePrenom, saisieNom);
+            listeContacts.push(nouveauContact);
+            console.log("Le nouveau contact a été ajouté");
+            break;
+        case "0":
+            console.log("Au revoir !");
+            break;
+        default:
+            console.log("Cette action n'est pas valide");
+    }
+}
